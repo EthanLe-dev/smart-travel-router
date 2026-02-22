2c3c7fa87774d197f48c08ea19139d09127f20ff
Le Ngoc Quy <nquyle2006@gmail.com>
2026-02-22 19:25:05 +0700

    first commit

---
+++ src/main/java/com/hackathon/smart_travel_router/SmartTravelRouterApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.smart_travel_router;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTravelRouterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTravelRouterApplication.class, args);
+	}
+
+}
